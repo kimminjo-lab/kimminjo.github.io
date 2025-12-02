06a7720148d6e825811cf45441c440b772d90aee
hannahkimminjo <memorichip@naver.com>
2025-12-03 01:09:49 +0900

    Add built files

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/memorichip.gitprofile/sw.js', { scope: '/memorichip.gitprofile/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
